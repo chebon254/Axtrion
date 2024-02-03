b607d846bb92916ca30a853076e7eaa50fd825d9
chebon254 <kelvinchebon90@gmail.com>
2024-02-03 21:02:36 +0300

    Setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import { Logo } from "./_components/logo";
+
+const AuthLayout = (
+    { children } : { children: React.ReactNode }
+) => {
+    return (
+        <div className="space-y-6 h-full flex flex-col items-center justify-center">
+            <Logo />
+            { children }
+        </div>
+    );
+};
+
+export default AuthLayout;
